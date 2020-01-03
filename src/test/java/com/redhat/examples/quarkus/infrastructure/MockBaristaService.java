24ac955df2378a5b2a7134d3c0e9ef7bea9f72db
Jeremy Davis <jeremyrdavis@mac.com>
2020-01-03 07:35:22 -0500

    updated Mocks

--- src/test/java/com/redhat/examples/quarkus/infrastructure/MockBaristaService.java
+++ src/test/java/com/redhat/examples/quarkus/infrastructure/MockBaristaService.java
@@ -12,6 +12,8 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Mock
+@ApplicationScoped
 public class MockBaristaService extends BaristaService {
 
     Logger logger = Logger.getLogger(MockBaristaService.class);
--- src/test/java/com/redhat/examples/quarkus/infrastructure/SendBaristaOrderTestCase.java
+++ src/test/java/com/redhat/examples/quarkus/infrastructure/SendBaristaOrderTestCase.java
@@ -3,14 +3,11 @@ package com.redhat.examples.quarkus.infrastructure;
 import com.redhat.examples.quarkus.CoffeeShop;
 import com.redhat.examples.quarkus.model.*;
 import io.quarkus.test.junit.QuarkusTest;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
 import javax.inject.Inject;
-import java.time.Duration;
 import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -19,16 +16,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @QuarkusTest
-public class SendKitchenOrderTest {
+public class SendBaristaOrderTestCase {
 
-    Logger logger = Logger.getLogger(SendKitchenOrderTest.class);
+    Logger logger = Logger.getLogger(SendBaristaOrderTestCase.class);
 
     @Inject
     CoffeeShop coffeeShop;
 
     @Test
     @Timeout(5)
-    public void testKitchenOrderIn() throws ExecutionException, InterruptedException {
+    public void testBaristaOrderIn() throws ExecutionException, InterruptedException {
 
         Order order = new Order();
         KitchenOrder kitchenOrder = new KitchenOrder(order, MenuItem.COOKIE);
@@ -42,5 +39,4 @@ public class SendKitchenOrderTest {
         assertEquals(OrderStatus.ACCEPTED, updatedOrder.status);
 
     }
-
 }
